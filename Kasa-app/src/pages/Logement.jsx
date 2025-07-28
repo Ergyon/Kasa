ceb8e60179382a8d5f60b491f404a64118faa6d1
Ergyon <benoitdoudard56@gmail.com>
2025-07-28 06:01:31 +0200

    Update Logement.jsx

--- Kasa-app/src/pages/Logement.jsx
+++ Kasa-app/src/pages/Logement.jsx
@@ -1,5 +1,20 @@
+import { useParams } from 'react-router-dom'
+import Error from '../components/Error'
+import datas from '../data/datas.json'
+
 const Logement = () => {
-  return <div>Logement</div>
+  const { id } = useParams()
+  const logement = datas.find((item) => item.id === id)
+
+  if (!logement) return <Error />
+
+  return (
+    <div className="housing">
+      <img src={logement.cover} alt={logement.title} className="housing__image" />
+      <h3 className="housing__title">{logement.title}</h3>
+      <span className="housing__location">{logement.location}</span>
+    </div>
+  )
 }
 
 export default Logement
