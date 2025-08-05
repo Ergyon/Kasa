cfe2a5dbf839b72ca7622f499050b55863626d50
Ergyon <benoitdoudard56@gmail.com>
2025-08-05 19:54:24 +0200

    responsive mobile

--- Kasa-app/src/components/Headline.jsx
+++ Kasa-app/src/components/Headline.jsx
@@ -4,7 +4,7 @@ import headlineCover from '../assets/img/headline-cover.png'
 const Headline = () => {
   return (
     <div className="headline" style={{ backgroundImage: `url(${headlineCover})` }}>
-      <h1 className="headline__title">Chez vous, et partout ailleurs</h1>
+      <h1 className="headline__title">Chez vous, partout et ailleurs</h1>
     </div>
   )
 }
